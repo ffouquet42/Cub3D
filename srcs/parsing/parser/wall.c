4572527b6479a46bf8f4074aafcbda1e2007cec4
ffouquet42 <ffouquet42@gmail.com>
2024-02-05 11:11:57 +0100

    start check if map is closed by wall

--- srcs/parsing/parser/wall.c
+++ srcs/parsing/parser/wall.c
@@ -6,7 +6,7 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 15:36:06 by fllanet           #+#    #+#             */
-/*   Updated: 2024/02/04 17:00:09 by fllanet          ###   ########.fr       */
+/*   Updated: 2024/02/05 11:10:44 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,19 @@
 
 int	closed_by_wall(t_data *data)
 {
-	(void)data;
-	return (0);
+	int	i;
+	printf("\n\n--- closed by wall start\n"); // dev
+	i = 0;
+	while (i < data->map_width)
+	{
+		if (data->map[0][i] != '1' && data->map[0][i] != ' ')
+		{
+			printf("map[0][%i] = %c\n", i, data->map[0][i]); // dev
+			return (ft_putstr("return 1\n", 1), 1); // dev
+		}
+		i++;
+	}
+	return (ft_putstr("return 0\n", 1), 0); // dev
 }
 
 /*
