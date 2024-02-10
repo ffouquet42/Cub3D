ed02cc9f6206a531bdfce77450f8d15e50e9f1e2
AK7iwi <atzlts@proton.me>
2024-02-10 21:26:32 +0100

    test

--- srcs/cub3D.c
+++ srcs/cub3D.c
@@ -6,16 +6,17 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 13:52:26 by fllanet           #+#    #+#             */
-/*   Updated: 2024/02/09 18:18:58 by mfeldman         ###   ########.fr       */
+/*   Updated: 2024/02/10 21:24:39 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3D.h"
 
 // dev (fonctions pour visualiser les structs dans le terminal pendant le dev, a supprimer avant de rendre le projet)
-void	print_scene(char **scene)
+void	print_scene(char **scene)  
 {
-	int i = 0;
+	int i;
+	i = 0;
 	while (scene[i])
 	{
 		printf("scene[%i] = %s", i, scene[i]);
