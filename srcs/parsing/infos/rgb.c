0f67e57c8c44930573b1f6ae4949a0e62f306b3d
AK7iwi <ATZLTS@proton.me>
2024-03-13 22:03:13 +0100

    are_rgb_valid to _is_rgb_vaid

--- srcs/parsing/infos/rgb.c
+++ srcs/parsing/infos/rgb.c
@@ -6,13 +6,13 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 10:13:34 by fllanet           #+#    #+#             */
-/*   Updated: 2024/03/11 20:05:03 by mfeldman         ###   ########.fr       */
+/*   Updated: 2024/03/13 22:02:55 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/cub3D.h"
 
-static inline	bool are_rgb_valid(char *rgb, uint8_t tab[RGB])
+static inline	bool is_rgb_valid(char *rgb, uint8_t tab[RGB])
 {
     uint8_t i;
 	uint8_t j;
@@ -24,14 +24,14 @@ static inline	bool are_rgb_valid(char *rgb, uint8_t tab[RGB])
 	while (rgb[++i])
 	{
 		j = 0;
-		if ((!is_digit(rgb[i]) && rgb[i] != ',' && rgb[i] != '\n') 
-				|| commas > 3)
-			return (false); // error
+		if (commas > 2)
+			return (false); 
 		tmp = ft_atoi(&rgb[i]);
 		while (is_digit(rgb[i++]))
 			j++;
 		i--;
-		if (j == 0 || j > 3 || tmp < 0 || tmp > 255)
+		if (j == 0 || j > 3 || tmp < 0 || tmp > 255 
+			|| (rgb[i] != ',' && rgb[i] != '\n'))
 			return (false); //error
 		else 
 			tab[commas] = tmp;
@@ -42,9 +42,9 @@ static inline	bool are_rgb_valid(char *rgb, uint8_t tab[RGB])
 
 bool 	parse_rgb(t_infos *infos)
 {
-	if (!are_rgb_valid(infos->infos[FLOOR], infos->rgb->rgb_floor)) // to muck value in the rgb_floor
+	if (!is_rgb_valid(infos->infos[FLOOR], infos->rgb->rgb_floor)) // to much value in the rgb_floor
 		return(EXIT_FAILURE);
-    if (!are_rgb_valid(infos->infos[CEILING], infos->rgb->rgb_ceiling))
+    if (!is_rgb_valid(infos->infos[CEILING], infos->rgb->rgb_ceiling))
 		return(EXIT_FAILURE);
 	return(EXIT_SUCCESS);
 }
